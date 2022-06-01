711fc827f7562207a3ed6ea8cd66f1892bb5c9c7
Luis Moreno <lucho_9722@hotmail.com>
2022-06-01 17:47:27 -0500

    segundo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,15 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var z=x+y
+  return z
 }
 
 function resta(x, y) {
